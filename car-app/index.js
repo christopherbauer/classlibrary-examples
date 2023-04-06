6dd440862eaa857fcdb6f10b0892e9d78e765405
Christopher Bauer <chrisbauer.career@gmail.com>
2023-04-05 19:43:34 -0600

    Add car app

---
+++ car-app/index.js
@@ -0,0 +1,10 @@
+const { NewCar } = require("@classlibrary/core-js");
+const car = new NewCar(10, 10);
+car.drive(18);
+car.drive(26);
+car.drive(10);
+car.drive(10);
+car.drive(30);
+car.drive(14);
+car.drive(10);
+car.drive(0);
